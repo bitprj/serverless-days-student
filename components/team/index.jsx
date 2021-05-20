7f488cb1c7d854ddc4a5e04ed8063163122823f8
Daniel Kim <dgkim@ucdavis.edu>
2021-05-19 22:25:23 -0700

    first comit

---
+++ components/team/index.jsx
@@ -0,0 +1,56 @@
+import { Box, Heading, SimpleGrid, Text } from '@chakra-ui/react'
+import * as React from 'react'
+import { Member } from './Member'
+import { members } from './_data'
+
+export const Team = () => (
+  <Box as="section">
+    <Box
+      mx="auto"
+      maxW={{
+        base: 'xl',
+        md: '7xl',
+      }}
+      px={{
+        base: '6',
+        md: '8',
+      }}
+      py={{
+        base: '12',
+        md: '20',
+      }}
+    >
+      <Box textAlign="center">
+        <Heading size="3xl" letterSpacing="tight" mb="5" fontWeight="extrabold">
+          Speakers
+        </Heading>
+        <Text fontSize="xl" maxW="2xl" mx="auto">
+          Serverless is a 
+        </Text>
+      </Box>
+      <SimpleGrid
+        mt="20"
+        columns={{
+          base: 1,
+          md: 2,
+          lg: 3,
+        }}
+        spacingX="6"
+        spacingY="16"
+      >
+        {members.map((member, idx) => (
+          <Member
+            key={idx}
+            role={member.role}
+            image={member.image}
+            name={member.name}
+            twitter="#"
+            linkedIn="#"
+          >
+            {member.description}
+          </Member>
+        ))}
+      </SimpleGrid>
+    </Box>
+  </Box>
+)
