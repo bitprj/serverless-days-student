bc6f15617b5205272afd6d8e24ea626fa66542db
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-21 15:50:00 -0400

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     let email = req.body.email
 
-    const response = await fetch(`https://api.mailerlite.com/api/v1/subscribers/107982808?` + new URLSearchParams({
+    const response = await fetch(`https://api.mailerlite.com/api/v1/subscribers/108061810?` + new URLSearchParams({
         apiKey: process.env.MAILERLITE_KEY,
         id: "108061810",
         email: email
