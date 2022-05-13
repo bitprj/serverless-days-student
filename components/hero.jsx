5b27aefd538069f8d28ae353c2b59d0b0507be7a
Ganning Xu <ganningxu@gmail.com>
2022-05-13 15:19:01 -0400

    redesign for 2022

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,128 +1,182 @@
 import {
-    Box,
-    Button,
-    Circle,
-    Heading,
-    Img,
-    SimpleGrid,
-    Stack,
-    Text,
-    VisuallyHidden,
-    useColorModeValue as mode,
-    LightMode,
-    Input,
-  } from '@chakra-ui/react'
-  import * as React from 'react'
-  import { FaPlay } from 'react-icons/fa'
-  
-  
-  export const Hero = ( { heading, description, cta1, cta2, image, play, cta1link, cta2link }) => {
-    const subscribeEmail = async event => {
-      event.preventDefault()
-      let email = document.getElementById('email').value
-      let emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-  
-      if (email == "") {
-        return;
-      }
-      else if (!emailRegex.test(email)) {
-        return;
-      }
-      console.log(`Subscribing ${email} to newsletter...`)
-  
-      const res = await fetch(`/api/subscribe`,
-        {
-          body: JSON.stringify({
-            email: email
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          method: 'POST'
-        }
-      )
-  
-      let result = await res.json()      
-      document.getElementById('email').remove()
-      document.getElementById('signup').remove()
-      document.getElementById('formResponse').innerText = "Thanks! See you soon ❤️"
-    
+  Box,
+  Button,
+  Circle,
+  Heading,
+  Img,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  useColorModeValue as mode,
+  Input,
+  FormControl,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { FaPlay } from "react-icons/fa";
+
+export const Hero = ({
+  heading,
+  description,
+  cta1,
+  cta2,
+  image,
+  play,
+  cta1link,
+  cta2link,
+}) => {
+  const subscribeEmail = async (event) => {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let emailRegex =
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if (email == "") {
+      return;
+    } else if (!emailRegex.test(email)) {
+      return;
     }
+    console.log(`Subscribing ${email} to newsletter...`);
 
-    return (
-      <Box cursor="url(/cursor.png), auto"> 
-        <Box as="section" bg="black" color="white" pt="7.5rem">
-          <Box maxW={{ base: 'xl', md: '7xl' }} mx="auto" px={{ base: '6', md: '8' }}>
-            <Box textAlign="center">
-              <Img mx="auto" maxW="48rem" width="100%" src="/serverless.png"></Img>
+    const res = await fetch(`/api/subscribe`, {
+      body: JSON.stringify({
+        email: email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "POST",
+    });
 
-              <Text fontSize="3xl" mt="4" maxW="xl" mx="auto" >
-                {description}
-              </Text>
-            </Box>
-  
-            <Stack
-              justify="center"
-              direction={{ base: 'column', md: 'row' }}
-              mt="10"
-              mb="5"
-              spacing="4"
-            >
-              <LightMode>
-              <form action="https://hopin.com/events/serverless-days-student-edition">
-              <Stack maxW="md" spacing="4" direction={{ base: 'column', sm: 'row' }}>
+    let result = await res.json();
+    document.getElementById("email").remove();
+    document.getElementById("signup").remove();
+    document.getElementById("formResponse").innerText =
+      "Thanks! See you soon ❤️";
+  };
 
-                <Button
+  return (
+    <Box cursor="url(/cursor.png), auto">
+      <Box as="section" bg="black" color="white" pt="7.5rem">
+        <Box
+          maxW={{ base: "xl", md: "7xl" }}
+          mx="auto"
+          px={{ base: "6", md: "8" }}
+        >
+          <Box textAlign="center">
+            <Img
+              mx="auto"
+              maxW="48rem"
+              width="100%"
+              src="/serverless.png"
+            ></Img>
+
+            <Text fontSize="3xl" mt="4" maxW="xl" mx="auto">
+              {description}
+            </Text>
+          </Box>
+
+          <Stack
+            justify="center"
+            direction={{ base: "column", md: "row" }}
+            mt="10"
+            mb="5"
+            spacing="4"
+          >
+            {/* <form action="https://hopin.com/events/serverless-days-student-edition">
+                <Stack
+                  maxW="md"
+                  spacing="4"
+                  direction={{ base: "column", sm: "row" }}
+                >
+                  <Button
                     type="submit"
                     id="signup"
                     size="lg"
                     colorScheme="purple"
                     px="12"
                     _hover={{
-                      cursor: "url(/cursorHover.png), auto"
+                      cursor: "url(/cursorHover.png), auto",
                     }}
                     fontWeight="bold"
                     fontSize="md"
                   >
-                  Join Us Now →
-                </Button>
+                    Join Us Now →
+                  </Button>
                 </Stack>
-              </form>
-              </LightMode>
-              
-            </Stack>
-            <Box textAlign="center" mb="25">
+              </form> */}
+            <Box display="flex" alignItems="center">
+              <FormControl mr="1rem" isRequired>
+                <Input
+                  id="email"
+                  rounded="0"
+                  placeholder="email"
+                  border="1px"
+                  borderColor="#404040"
+                  bg="#404040"
+                  _hover=""
+                  _focus=""
+                  color="white"
+                  _placeholder={{ color: "white" }}
+                />
+              </FormControl>
+              <Button
+                id="signup"
+                onClick={subscribeEmail}
+                bg="#9F16CE"
+                px="8"
+                rounded="0"
+                color="white"
+                fontWeight="bold"
+                fontSize="md"
+                _hover={{
+                  background: "#6c0d8c",
+                }}
+              >
+                Sign Up
+              </Button>
 
-            <Text id="formResponse" fontSize="3xl" mt="4" maxW="xl" mx="auto" >
-            </Text>
+              <Box textAlign="center">
+                <Text
+                  id="formResponse"
+                  fontSize="lg"
+                  mt="4"
+                  maxW="xl"
+                  mx="auto"
+                />
+              </Box>
+            </Box>
+          </Stack>
+          <Box textAlign="center" mb="25">
+            <Text
+              id="formResponse"
+              fontSize="3xl"
+              mt="4"
+              maxW="xl"
+              mx="auto"
+            ></Text>
             <br></br>
             <Text fontSize="2xl" maxW="xl" mx="auto" color="gray.300">
-                August 15th, 2021 | Online
-              </Text>
-            </Box>
+              August 2022 | Online
+            </Text>
+          </Box>
 
-            <Box
-              mb={{ base: '-20', md: '-40' }}
-              className="group"
-              position="relative"
-              rounded="lg"
-              overflow="hidden"
-            >
-              <center>
-              <Img
-                alt="Campers"
-                src="/becks.svg"
-                width="80%"
-              />
-              </center>
-            </Box>
+          <Box
+            mb={{ base: "-20", md: "-40" }}
+            className="group"
+            position="relative"
+            rounded="lg"
+            overflow="hidden"
+          >
+            <center>
+              <Img alt="Campers" src="/becks.svg" width="80%" />
+            </center>
           </Box>
         </Box>
-  
-        <Box as="section" pt={{ base: '16', md: '40' }} >
-        </Box>
       </Box>
-    )
-  }
-  
+
+      <Box as="section" pt={{ base: "16", md: "40" }}></Box>
+    </Box>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import * as React from 'react'
-import { Hero } from './../components/hero'
-import { Alert } from './../components/alert'
-import { Features } from './../components/feature'
-import { Footer } from './../components/footer/index'
-import { Team } from './../components/team'
-import { Sponsors } from './../components/sponsors'
-import { Workshop } from '../components/workshop'
-import {
-  Box,
-} from '@chakra-ui/react'
-
+import Head from "next/head";
+import Image from "next/image";
+import * as React from "react";
+import { Hero } from "./../components/hero";
+// import { Alert } from "./../components/alert";
+import { Features } from "./../components/feature";
+// import { Footer } from "./../components/footer/index";
+// import { Team } from "./../components/team";
+// import { Sponsors } from "./../components/sponsors";
+// import { Workshop } from "../components/workshop";
+import { Heading } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
-
-  const playSound = () => {
-    let audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
-  }
+  // const playSound = () => {
+  //   let audioEl = document.getElementsByClassName("audio-element")[0];
+  //   audioEl.play();
+  // };
 
   return (
-    <Box cursor="url(/cursor.png), auto" onClick={() => playSound()} bg="black">
+    // <Box cursor="url(/cursor.png), auto" onClick={() => playSound()} bg="black">
+    <Box cursor="url(/cursor.png), auto" bg="black">
       <Head>
         <title>Serverless Days: Student Edition</title>
-        <meta name="description" content="We are a virtual conference for students, by students learning serverless" />
+        <meta
+          name="description"
+          content="We are a virtual conference for students, by students learning serverless"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <Alert /> */}
@@ -34,28 +35,27 @@ export default function Home() {
         cta2="RSVP →"
         image="/serverless.png"
         cta1link="https://www.notion.so/bitproject/Welcome-to-Serverless-Camp-e218f86daf4248509350709cd9fa8017"
-
       />
 
-      <Features 
+      <Features
         title="About the conference"
         p1="Bit Project is super excited to announce Serverless Days : Student Edition, a virtual conference for folks starting their journey with serverless."
-        p2="From boot campers to experienced engineers who want to break into serverless, this conference will showcase unique serverless projects as well as tips and tricks for getting started." 
+        p2="From boot campers to experienced engineers who want to break into serverless, this conference will showcase unique serverless projects as well as tips and tricks for getting started."
         img="/octo.png"
       />
 
-      <Workshop />
+      <Heading size="xl" py={12} fontWeight="bold" textAlign="center">
+        More details coming soon!
+      </Heading>
+
+      {/* <Workshop />
       <Team />
       <Sponsors />
-      <Footer />
-
-
+      <Footer /> */}
 
       <audio className="audio-element">
-          <source src="/meow.wav"></source>
-        </audio>
-
-
+        <source src="/meow.wav"></source>
+      </audio>
     </Box>
-  )
+  );
 }
