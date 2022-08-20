adfa66cd74d3a6700ab80ae08664e8efb29a81b6
Ganning Xu <ganningxu@gmail.com>
2022-08-20 11:32:27 -0400

    added and fixed workshop timse

--- pages/index.js
+++ pages/index.js
@@ -54,17 +54,38 @@ export default function Home() {
           >
             <TalkCard
               day={1}
+              type="keynote"
+              time="8/20 @ 10:00 AM PT"
+              title="Serverless Education & CounselorBot"
+              desc="Ganning and Emily share their experiences teaching Serverless to students across the United States. We'll also talk about CounselorBot!"
+              names={["Ganning Xu", "Emily Chen"]}
+              avatars={["/ganning.jpeg", "/emily.jpeg"]}
+            />
+
+            <TalkCard
+              day={1}
               type="workshop"
-              time="8/20 @ 2:00 PM PT"
+              time="8/20 @ 11:30 AM PT"
               title="Build Your Tech Portfolio with John Kilmister"
               desc="There are three crucial tools that a software engineer needs to break into tech: GitHub, LinkedIn, and a Resume. With 17+ years of experience working in software, John Kilmister will share his thoughts on how to make your technical portfolio stand out in job applications."
               names={["John Kilmister"]}
               avatars={["/john.jpeg"]}
             />
+
+            <TalkCard
+              day={1}
+              type="workshop"
+              time="8/20 @ 3:00 PM PT"
+              title="Introduction to APIs by Shreya Gupta"
+              desc="In this Intro to APIs workshop, we will break down what an API is, understand how engineers use them, and explore the limitless technical solutions that they provide. We will also discuss how and why APIs are crucial to software engineering teams at startups."
+              names={["Shreya Gupta"]}
+              avatars={["/shreya.jpeg"]}
+            />
+
             <TalkCard
               day={1}
               type="talk"
-              time="8/20 @ 3:00 PM PT"
+              time="8/20 @ 1:00 PM PT"
               title="Developing and Deploying Secure Code with AWS Lambda with Tal Melamed"
               desc="Tal has over 15 years experience in security research and engineering and recently co-founded CloudEssence, a cloud-native security company that enables organizations to extend security visibility to applications developed in a serverless architecture. In 2020, CloudEssence was acquired by Contrast Security, where he currently leads the new research center in Italy. Previous to CloudEssence, Tal was head of security research at Protego Labs, a Serverless security start-up that was acquired by Check Point in 2019."
               names={["Tal Melamed"]}
@@ -74,37 +95,17 @@ export default function Home() {
             <TalkCard
               day={1}
               type="talk"
-              time="8/20 @ 5:00 PM PT"
-              title="Intro to React through Pokémon with Bryan Wong"
-              desc="Learning the basics of React and Es6 by building a Pokédex from Pokémon!"
-              names={["Bryan Wong"]}
-              avatars={["bryan.jpeg"]}
+              time="8/20 @ 1:00 PM PT"
+              title="Serverless Web Apps with Jamstack with Anthony Chu"
+              desc="Serverless is more than just functions. The Jamstack architecture is a serverless approach to building web apps that are fast, scalable, and efficient. Join us in this session to learn how to use serverless technologies to power your entire web stack."
+              names={["Anthony Chu"]}
+              avatars={["/anthony.jpeg"]}
             />
 
             <TalkCard
               day={1}
-              type="workshop"
-              time="8/20 @ 3:00 PM PT"
-              title="Using Drone to Deploy Cloud Native Apps with Confidence with Jaap Brasser"
-              desc="Over the past few years, a growing number of organizations have started to adopt Cloud Native architectures. Despite the adoption of Cloud Native architectures many companies haven’t achieved optimal results. Wondering why? One of  the reasons is our adherence to traditional ways of building and deploying applications."
-              names={["Jaap Brasser"]}
-              avatars={["/jaap.jpeg"]}
-            />
-
-            <TalkCard
-              day={2}
               type="talk"
-              time="8/21 @ 3:00 AM PT"
-              title="Deploy Your Azure Functions the DevOps Way with Jaap Brasser"
-              desc="So you're using Azure Functions, but how can we make sure that everything runs as intended when it's deployed. Using proper CI/CD pipelines can be beneficial but hard to setup. Join us in this session to learn about the power of automated pipelines to create more, faster!"
-              names={["Jaap Brasser"]}
-              avatars={["/jaap.jpeg"]}
-            />
-
-            <TalkCard
-              day={2}
-              type="talk"
-              time="8/21 @ 3:00 AM PT"
+              time="8/21 @ 1:30 PM PT"
               title="How to Write Node.js Lambda Functions with Luke Hedger"
               desc="In this talk, Luke will walk-through how to write the most awesome Node.js Lambda functions, from project initialization through to deployment and operation. We'll explore folder structure, code standards, dependency management, linting, formatting, static analysis, testing, CI/CD and monitoring. All by employing the latest and greatest standards, as used by serverless experts! We'll also question what we could be doing better and what the future might hold."
               names={["Luke Hedger"]}
@@ -112,59 +113,19 @@ export default function Home() {
             />
 
             <TalkCard
-              day={2}
+              day={1}
               type="workshop"
-              time="8/21 @ 3:00 AM PT"
-              title="Learn to Manage Templates for Serverless Applications with Tatiana Cooke"
-              desc="DevOps and Platform Engineers are experts in infrastructure as code (IaC) who are responsible for helping their organization implement design best-practices in the cloud. As serverless emerges as a preferred technology, having the right tools to manage IaC templates can make a big difference. In this workshop, we'll cover AWS tools including CloudFormation for IaC templating, and AWS Proton for template management to give you an idea of how DevOps engineers are engaging with this type of technology."
-              names={["Tatiana Cooke"]}
-              avatars={["/tatiana.png"]}
-            />
-
-            <TalkCard
-              day={2}
-              type="talk"
-              time="8/21 @ 3:00 AM PT"
-              title="Serverless: Ship on Day 1 with Luke Hedger"
-              desc="Unleash the full power of serverless by shipping your software on day 1 - and every day after that! Let's look at how we can snap together a small set of tools to quickly and simply get your serverless application running in the AWS cloud. Featuring: VS Code, GitHub and AWS. We will explore the basics of local development, infrastructure as code, automated tests, continuous delivery pipelines and observability!"
-              names={["Luke Hedger"]}
-              avatars={["/luke.jpeg"]}
-            />
-
-            <TalkCard
-              day={2}
-              type="talk"
-              time="8/21 @ 10:00 AM PT"
-              title="Serverless Education & CounselorBot"
-              desc="Ganning and Emily share their experiences teaching Serverless to students across the United States. We'll also talk about CounselorBot!"
-              names={["Ganning Xu", "Emily Chen"]}
-              avatars={["/ganning.jpeg", "/emily.jpeg"]}
-            />
-
-            <TalkCard
-              day={2}
-              type="talk"
-              time="8/21 @ 10:00 AM PT"
-              title="Why building serverless applications makes you a better developer!"
-              desc="Not only does serverless allow you to produce results faster with lower upfront and maintenance costs and hassle, it can even help you in building better, more robust distributed applications without even trying."
-              names={["Gareth McCumskey"]}
-              avatars={["/gareth.jpeg"]}
-            />
-
-            <TalkCard
-              day={2}
-              type="talk"
-              time="8/21 @ 10:00 AM PT"
-              title="Why building serverless applications makes you a better developer!"
-              desc="Not only does serverless allow you to produce results faster with lower upfront and maintenance costs and hassle, it can even help you in building better, more robust distributed applications without even trying."
-              names={["Gareth McCumskey"]}
-              avatars={["/gareth.jpeg"]}
+              time="8/20 @ 2:00 PM PT"
+              title="Intro to React through Pokémon with Bryan Wong"
+              desc="Learning the basics of React and Es6 by building a Pokédex from Pokémon!"
+              names={["Bryan Wong"]}
+              avatars={["bryan.jpeg"]}
             />
 
             <TalkCard
               day={2}
               type="workshop"
-              time="8/21 @ 1:00 PM PT"
+              time="8/21 @ 10:00 AM PT"
               title="API Integration Demo and Debugging Tutorial with Christian Lechner"
               desc="Integrating software solutions into your webapp can take your projects to the next level in a matter of minutes. Development Architect, Christian Lechner, brings his experience as an Azure MVP to demonstrate the fundamentals of incorporating the GitHub and Dev.to APIs into your Azure HTTP-Trigger Functions."
               names={["Christian Lechner"]}
@@ -174,11 +135,101 @@ export default function Home() {
             <TalkCard
               day={2}
               type="talk"
-              time="8/21 @ 2:00 PM PT"
+              time="8/21 @ 11:30 AM PT"
               title="Typescript Beyond The Stack with the CDK with Ryan Cormack"
               desc="The modern technology stack traditionally consists of the Frontend, Backend and a Database. But we need a way to get our code from our computer to their server. The AWS Cloud Developer Kit (CDK) is a tool that allows us to write and deploy our infrastructure in languages used to write the rest of the stack, including Typescript. By leveraging the CDK we’re able to write our infrastructure, backend and frontend all in Typescript and even deploy it to Amazon’s serverless compute platform. In this session Ryan will look at the basic concepts behind the CDK, what is happening when you use it and how it allows us to quickly and easily get a full stack Typescript application, with a website, API and database, up and running in a language we are familiar with."
               names={["Ryan Cormack"]}
               avatars={["/ryan.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 12:00 PM PT"
+              title="Function Orchestration in the world of Serverless by Daron Yöndem"
+              desc="You have a function, or two, or maybe more? Now is the time to talk about making sure you have full control on how data and communication flows between your functions. You need something to control how your functions interact to each other. You need something that can give you granular control, or sometimes maybe a simple user interface to get it done. This session is about your options in Azure to accomplish all that. We will look into Durable Functions and Logic Apps to understand where they can help us scaling our serverless move."
+              names={["Ryan Cormack"]}
+              avatars={["/ryan.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 12:30 PM PT"
+              title="Business Card Storer and Information Extraction with Azure Functions by Melody Lee"
+              desc="The accumulation of business cards from years of networking and travels is inevitable. The storage of these cards can become a hassle, especially as they start to pile up on desks and in wallets. This talk discusses a solution to this issue: a simple SMS-based upload of these business cards to Cosmos DB and extraction of relevant information from the card and online, all of which is then returned to the user."
+              names={["Melody Lee"]}
+              avatars={["/melody.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 12:45 PM PT"
+              title="Helping Purdue Students poop! by Jalynn Chang"
+              desc="Take a look at my final project from Serverless Camp. I created a react app with a front end and backend hosted on Azure. My app stores lists of books, movies, songs, etc... and is integrated with online media collections like Imdb, Google Books, and Spotify. "
+              names={["Jalynn Chang"]}
+              avatars={["/jalynn.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 1:00 PM PT"
+              title="Media Collection Web App by Jack Shunn"
+              desc="Take a look at my final project from Serverless Camp. I created a react app with a front end and backend hosted on Azure. My app stores lists of books, movies, songs, etc... and is integrated with online media collections like Imdb, Google Books, and Spotify."
+              names={["Jack Shunn"]}
+              avatars={["/jack.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 1:15 PM PT"
+              title="Tired of Making Travel Plans? Try the Newest Trip Generator! by Noah Zhang"
+              desc="This website would randomly generate a one-day trip plan for you, based on your inputs of location, favorite attraction categories, transportation method, etc. The trip plan includes multiple destinations' basic information. Between different destinations, there will be a link to Google Map that shows the navigation route between them."
+              names={["Noah Zhang"]}
+              avatars={["/noah.png"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 1:30 PM PT"
+              title="When To Leave For An Event Project by Sania Khaja"
+              desc="Take a look at my final project from Serverless Camp. I created a react app with a front end and backend hosted on Azure. My app stores lists of books, movies, songs, etc... and is integrated with online media collections like Imdb, Google Books, and Spotify."
+              names={["Sania Khaja"]}
+              avatars={["/sania.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 1:45 PM PT"
+              title="Saving Money at the Grocery Store with Serverless! by Matthew Morales"
+              desc="I'll be presenting my final project as part of BitProject's Serverless Camp! My app is a simple tool to help save money on food by planning shopping trips ahead of time."
+              names={["Matthew Morales"]}
+              avatars={["/matthew.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 2:00 PM PT"
+              title="Serverless: Ship on Day 1 with Luke Hedger"
+              desc="Unleash the full power of serverless by shipping your software on day 1 - and every day after that! Let's look at how we can snap together a small set of tools to quickly and simply get your serverless application running in the AWS cloud. Featuring: VS Code, GitHub and AWS. We will explore the basics of local development, infrastructure as code, automated tests, continuous delivery pipelines and observability!"
+              names={["Luke Hedger"]}
+              avatars={["/luke.jpeg"]}
+            />
+
+            <TalkCard
+              day={2}
+              type="talk"
+              time="8/21 @ 2:30 PM PT"
+              title="Closing Remarks with Shreya Gupta"
+              desc="Wrap up on the talks, workshops, and the 2022 Serverless Days: Student Edition conference."
+              names={["Shreya Gupta"]}
+              avatars={["/shreya.jpeg"]}
             />
           </SimpleGrid>
           <MedSep line={true} />
